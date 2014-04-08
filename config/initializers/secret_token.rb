ee37aea4140a320e90688e2e5fa76153c8b39831
Marc Cyr <marcacyr@gmail.com>
2014-04-08 09:52:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudEdit::Application.config.secret_token = '40bd8166601bba020b642fe73fc8fc01b8c7aa13791d3c96de836da1ba696d6543dbcf44152f20c21188ba61458cadae9b7d03a9b4588018d4b310731d3de81e'
